557510f420fdb9c6861849cac6190cbd8ac0629e
Patrick <cowinkkeydinkinc@gmail.com>
2022-07-21 01:46:31 -0600

    Use container registration
    Signed-off-by: Patrick <cowinkkeydinkinc@gmail.com>

--- core/src/main/java/tc/oc/pgm/loot/LootMatchModule.java
+++ core/src/main/java/tc/oc/pgm/loot/LootMatchModule.java
@@ -2,7 +2,9 @@ package tc.oc.pgm.loot;
 
 import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import org.bukkit.Material;
@@ -10,9 +12,11 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.api.match.MatchModule;
 import tc.oc.pgm.api.match.MatchScope;
@@ -24,16 +28,28 @@ import tc.oc.pgm.filters.query.BlockQuery;
 
 @ListenerScope(MatchScope.RUNNING)
 public class LootMatchModule implements MatchModule, Listener {
+
+  // Map of containers to a flag indicating whether they contained objective wool when the match
+  // started.
+  // For this to work, containers have to be checked for wool before their contents can be changed.
+  // To ensure this,
+  // containers are registered in this map the first time they are opened or accessed by a hopper or
+  // dispenser.
+  private final Map<Inventory, Boolean> containers = new HashMap<>();
+
+  // Containers which have cached items at match start
+  // have an entry in this map representing the exact
+  // layout of the items in the inventory. This is used to refill the container.
+  private final Map<Inventory, Map<Integer, ItemStack>> cachedContainers = new HashMap<>();
+
   private final Match match;
   private final List<LootableDefinition> definitions;
-  private final List<FillableCache> fillableCaches;
   private final List<LootCountdown> lootCountdowns;
   private final CountdownContext countdownContext;
 
   public LootMatchModule(Match match, List<LootableDefinition> definitions) {
     this.match = match;
     this.definitions = definitions;
-    this.fillableCaches = new ArrayList<>(this.definitions.size());
     this.lootCountdowns = new ArrayList<>(this.definitions.size());
     this.countdownContext = new CountdownContext(match, match.getLogger());
   }
@@ -67,11 +83,89 @@ public class LootMatchModule implements MatchModule, Listener {
     }
   }
 
+  private void registerContainer(Inventory inv, Block block) {
+    // When a chest (or other block inventory) is accessed, check if it's a cache chest
+    Boolean isCacheChest = this.containers.get(inv);
+    if (isCacheChest == null) {
+      // If we haven't seen this chest yet, check it for cache
+      isCacheChest = this.isCacheChest(block);
+      this.containers.put(inv, isCacheChest);
+      if (isCacheChest) {
+        // If it is a cached chest, take a snapshot of the items
+        Map<Integer, ItemStack> contents = new HashMap<>();
+        this.cachedContainers.put(inv, contents);
+        for (int slot = 0; slot < inv.getSize(); ++slot) {
+          ItemStack stack = inv.getItem(slot);
+          if (stack != null) {
+            contents.put(slot, stack.clone());
+          }
+        }
+      }
+    }
+  }
+
+  private boolean isCacheChest(Block block) {
+    for (LootableDefinition definition : definitions) {
+      if (definition.getCache() != null) {
+        if (definition.getCache().getRegion().query(new BlockQuery(block)).isAllowed()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  public void onContainerPlace(BlockPlaceEvent event) {
+    // Blacklist any placed container blocks
+    if (event.getBlock().getState() instanceof InventoryHolder) {
+      this.containers.put(((InventoryHolder) event.getBlock().getState()).getInventory(), false);
+    }
+  }
+
+  /*
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+  public void onItemTransfer(InventoryMoveItemEvent event) {
+    // When a hopper or dispenser transfers an item, register both blocks involved
+    //TODO find out how to get the blocks from this event, casts doesn't work
+    this.registerContainer(event.getSource(), (Block) event.getSource().getHolder());
+    this.registerContainer(event.getDestination(), (Block) event.getDestination().getHolder());
+  }
+   */
+
+  // function for adding items from <cache>, places items in the exact same pattern
+  private void refillContainers() {
+    // for each cachedContainer, which contains both a map of inventory and map of slotEntries
+    for (Map.Entry<Inventory, Map<Integer, ItemStack>> container :
+        this.cachedContainers.entrySet()) {
+      Inventory inv = container.getKey();
+      // for each slotEntry (which contains an index number and item) in the container
+      for (Map.Entry<Integer, ItemStack> slotEntry : container.getValue().entrySet()) {
+        int slot = slotEntry.getKey();
+        // from cached containers
+        ItemStack cachedItem = slotEntry.getValue();
+        // item (or empty space) retrieved from inventory
+        ItemStack retrievedItem = inv.getItem(slotEntry.getKey());
+
+        if (retrievedItem == null) {
+          retrievedItem = cachedItem.clone();
+          inv.setItem(slot, retrievedItem);
+          break;
+        } else if (retrievedItem.isSimilar(cachedItem)
+            && retrievedItem.getAmount() < cachedItem.getAmount()) {
+          retrievedItem.setAmount(retrievedItem.getAmount() + 1);
+          inv.setItem(slot, retrievedItem);
+          break;
+        }
+      }
+    }
+  }
+
   @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
   public void onInventoryOpen(InventoryOpenEvent event) {
     Block clickedMaterial = event.getPlayer().getTargetBlock((Set<Material>) null, 5);
-    // currently does not worth with chest/furnace/dropper minecarts
     if (clickedMaterial.getState() instanceof InventoryHolder) {
+      this.registerContainer(event.getInventory(), clickedMaterial);
       MatchPlayer matchPlayer = match.getPlayer(event.getPlayer());
       Inventory containerInventory = event.getInventory();
       for (LootableDefinition definition : definitions) {
@@ -98,21 +192,8 @@ public class LootMatchModule implements MatchModule, Listener {
             }
           }
         }
-        // Find all Caches that the opened inventory is part of
-        if (definition.getCache() != null) {
-          // if chest is in region and passes filter, fix this
-          if (definition.getCache().getFilter().query(matchPlayer).isAllowed()) {
-            FillableCache fillableCache =
-                new FillableCache(containerInventory, definition.getId(), definition.getCache());
-            if (fillableCaches.contains(fillableCache)) {
-              containerInventory = fillableCache.getInventory();
-            } else {
-              this.fillableCaches.add(
-                  new FillableCache(containerInventory, definition.getId(), definition.getCache()));
-            }
-          }
-        }
         // start refill interval
+        // TODO fix countdowns or implement them as a separate function somewhere else
         for (LootCountdown countdown : lootCountdowns) {
           if (countdown.getLootableDefinition().equals(definition)) {
             this.countdownContext.start(countdown, definition.refillInterval);
